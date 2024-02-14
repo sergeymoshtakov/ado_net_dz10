72e83cf888e0518ab1f59fe64b53a6f94832591f
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-02-14 20:59:35 +0100

    Добавьте файлы проекта.

---
+++ MyMiddlewareLibrary/ErrorLoggingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace MyMiddlewareLibrary
+{
+    public class ErrorLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _logFilePath;
+
+        public ErrorLoggingMiddleware(RequestDelegate next, string logFilePath)
+        {
+            _next = next;
+            _logFilePath = logFilePath;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await LogErrorAsync(context, ex);
+                throw;
+            }
+        }
+
+        private async Task LogErrorAsync(HttpContext context, Exception exception)
+        {
+            string logMessage = $"[{DateTime.UtcNow}] {context.Request.Path}: {exception.Message}";
+            await WriteToLogFile(logMessage);
+        }
+
+        private async Task WriteToLogFile(string logMessage)
+        {
+            try
+            {
+                await File.AppendAllTextAsync(_logFilePath, logMessage + Environment.NewLine);
+            }
+            catch (Exception) {}
+        }
+    }
+}
+
---
+++ MyMiddlewareLibrary/ErrorLoggingMiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMiddlewareLibrary
+{
+    public static class ErrorLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseErrorLoggingMiddleware(this IApplicationBuilder builder, string logFilePath)
+        {
+            return builder.UseMiddleware<ErrorLoggingMiddleware>(logFilePath);
+        }
+    }
+}
